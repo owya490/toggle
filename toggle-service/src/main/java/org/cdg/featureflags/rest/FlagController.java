97247de19e25eab0269e4f3cc4be1208a1db1db2
Owen <owen.yang@sowaustralia.com>
2023-08-30 03:07:25 +1000

    updated ssl

--- toggle-service/src/main/java/org/cdg/featureflags/rest/FlagController.java
+++ toggle-service/src/main/java/org/cdg/featureflags/rest/FlagController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Optional;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping(
         path = "/flag",
         produces = MediaType.APPLICATION_JSON_VALUE,
